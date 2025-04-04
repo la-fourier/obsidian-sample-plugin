efbad9dd6422bf1b39dad4320dfa53d43a8756f0
Johannes Loos <jojoi7@gmx.de>
2025-04-05 00:34:17 +0200

    gierige änderungen, Tesseract integriert :)

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { chdir } from 'process';
 
 // Remember to rename these classes and interfaces!
 
@@ -12,21 +13,72 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	lit_link: boolean;
+	ribbon_lit_link: HTMLElement;
 
 	async onload() {
 		await this.loadSettings();
+		this.lit_link = false;
+
+		this.ribbon_lit_link = this.addRibbonIcon('droplets', 'Lit Link', async (evt: MouseEvent) => {
+			this.lit_link = !this.lit_link;
+			if (this.lit_link) {
+				this.ribbon_lit_link?.classList.add("is-active");
+			} else {
+				this.ribbon_lit_link?.classList.remove("is-active");
+			}
+			while (this.lit_link) {
+				const initialClipboard = await navigator.clipboard.readText();
+				let newClipboard = initialClipboard;
+
+				// console.log("Warte auf Änderungen im Clipboard...");
+				while (newClipboard === initialClipboard && this.lit_link) {
+					await new Promise(resolve => setTimeout(resolve, 400));
+					newClipboard = await navigator.clipboard.readText();
+				}
+				await this.link_literature(newClipboard);
+			}
+			console.log(`lit link beendet`);
+		});
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new SampleSettingTab(this.app, this));
+
+		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// Using this function will automatically remove the event listener when this plugin is disabled.
+		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+			console.log('click', evt);
+		});
+
+		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	}
+
+	async onload_() {
+		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('pencil', 'Lit Link', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
+			const fs = require('fs');
+			const path = 'orga/forms/task.md';
+
+			fs.appendFile(this.app.vault.getFileByPath(path)!.toString(), '\nHallo!', (err: Error) => {
+				if (err) {
+					console.error('Fehler beim Anhängen an die Datei:', err);
+				} else {
+					console.log('"Hallo!" wurde erfolgreich angehängt.');
+				}
+			});
+
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// const statusBarItemEl = this.addStatusBarItem();
+		// statusBarItemEl.setText('Status Bar Text');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
@@ -44,6 +96,13 @@ export default class MyPlugin extends Plugin {
 				console.log(editor.getSelection());
 				editor.replaceSelection('Sample Editor Command');
 			}
+		});
+		this.addCommand({
+			id: 'lit-link-worflow',
+			name: 'link literature',
+			editorCallback: () => this.watchClipboardAndSave(),
+			// write to outline
+			// replace img name
 		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
@@ -78,6 +137,54 @@ export default class MyPlugin extends Plugin {
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
+	link_literature(clipboard: string) {
+		let active = this.app.workspace.getActiveFile()!;
+
+		const file = this.app.vault.getFileByPath(active.path.replace(".md", "-outline.md"))!;
+
+		this.app.vault.append(file, clipboard);
+
+		this.waitForFocusChange();
+	}
+
+	async watchClipboardAndSave() {
+		const initialClipboard = await navigator.clipboard.readText();
+		let newClipboard = initialClipboard;
+
+		// console.log("Warte auf Änderungen im Clipboard...");
+		while (newClipboard === initialClipboard) {
+			await new Promise(resolve => setTimeout(resolve, 400));
+			newClipboard = await navigator.clipboard.readText();
+		}
+
+
+		let active = this.app.workspace.getActiveFile()!;
+
+		console.log(active.basename, active.name, active.path);
+
+		const fileName = `/${active.path}-outline.md`;
+		const filePath = `/${fileName}`;
+		await this.app.vault.create(filePath, newClipboard);
+
+		// this.app.fileManager.renameFile();
+
+		console.log(`Inhalt gespeichert in: ${filePath}`);
+
+		console.log("Warte auf Fensterwechsel...");
+		await this.waitForFocusChange();
+		console.log(`Fenster gewechselt. Datei: ${filePath}`);
+	}
+
+	waitForFocusChange(): Promise<void> {
+		return new Promise(resolve => {
+			const handleFocus = () => {
+				window.removeEventListener("blur", handleFocus);
+				resolve();
+			};
+			window.addEventListener("blur", handleFocus);
+		});
+	}
+
 	onunload() {
 
 	}
@@ -89,6 +196,11 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async linklitflow() {
+		// wait for pdf+ or clipboard change
+		// 
+	}
 }
 
 class SampleModal extends Modal {
@@ -97,12 +209,12 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.setText('Woah!');
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -116,7 +228,7 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
@@ -130,5 +242,11 @@ class SampleSettingTab extends PluginSettingTab {
 					this.plugin.settings.mySetting = value;
 					await this.plugin.saveSettings();
 				}));
+
+		// Setting für lit folder
+		// Setting für outline folder
+		// ggf template für name, sonst halt unsauber gecoded
+		// ggf Ordner für Screenshots
+		// ggf template für Screenshotbenennung
 	}
 }
